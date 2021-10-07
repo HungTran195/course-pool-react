1c969cfb66ddbd733310a8f8ea5849210d1a0709
Hung Quoc Tran <quochung.064@gmail.com>
2021-10-07 13:57:34 -0500

    update suggest new course page

--- frontend/src/pages/SuggestCourse/SuggestCoursePage.js
+++ frontend/src/pages/SuggestCourse/SuggestCoursePage.js
@@ -1,48 +1,99 @@
-import React, { Component } from "react";
-import HeroSectionForSuggest from "./HeroSectionForSuggest"
-export default class SuggestCoursePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: "",
-            email: "",
-            courseName: "",
-            courseURL: "",
-            description: "",
-        };
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.handleFormChange = this.handleFormChange.bind(this);
-    }
-    handleFormChange(event) {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-        console.log(name, value);
-        this.setState({ [name]: value });
-    }
+import React, { useState, useEffect } from "react";
+import HeroSectionForSuggest from "./HeroSectionForSuggest";
 
-    handleFormSubmit(event) {
-        // TODO: send sugegstion to server 
-        alert("Form submitted!" + this.state.name + this.state.email);
+const {REACT_APP_BASE_URL} = process.env;
+
+const SuggestCoursePage = () => {
+    document.title = "Course Pool | Suggest Your Favorite Course";
+    
+    const initialState = {
+        name: '',
+        email: '',
+        course_name: '',
+        course_url: '',
+        description: ''
+    };
+    const [suggestCourse, setSuggestCourse] = useState(initialState);
+    const [submitStatus, setSubmitStatus] = useState({
+        isSuccess: false,
+        msg: initialState
+    });
+
+    const handleFormChange = (event) => {
+        const {name, value} = event.target;
+
+        setSuggestCourse({
+            ...suggestCourse,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = (event) => {
+        setSubmitStatus({
+            isSuccess: false,
+            msg: initialState
+        });
+        const FETCH_URL = REACT_APP_BASE_URL + '/api/add-course';
+        fetch(FETCH_URL, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(suggestCourse),
+        }).then(res => {
+            if(res.status === 201){
+                setSubmitStatus(prevStatus => ({
+                    ...prevStatus,                    
+                    isSuccess: true
+                }));
+                return;
+            }
+            else return res.json()
+        }).then(data => {
+            if(data) {
+                for(const [name,  msg ] of Object.entries(data)){
+                    setSubmitStatus(prevStatus => ({
+                        ...prevStatus,
+                        isSuccess: false,
+                        msg:{
+                            ...prevStatus.msg,
+                            [name]: msg,
+                        }
+                    }));
+                };
+            }
+        }).catch(e => console.log('error', e));
+
+
         event.preventDefault();
-    }
+    };
 
-    renderSuggestCoursePage = () => {
-        return (
+    return (
+        <>
+            <HeroSectionForSuggest />
+            <div className="container">
             <div id="forms" className="pb-3 pt-3 pb-5">
-                <form className="form mx-auto" onSubmit={this.handleFormSubmit}>
+                <form className="form mx-auto" onSubmit={handleFormSubmit}>
                     <div className="input-group row">
                         <label
-                            className="col-lg-2 col-md-1 col-form-label form-control-label text-light justify-content-end align-items-center d-lg-flex ">Name</label>
+                            className="col-lg-2 col-md-1 col-form-label form-control-label text-light justify-content-end align-items-start d-lg-flex ">Name</label>
                         <div className="col-md-5 col-lg-4">
                             <input className="form-control mb-2" id="id_name" type="text" name="name"
-                                placeholder="James" value={this.name} onChange={this.handleFormChange} />
+                                placeholder="James" value={suggestCourse.name} onChange={handleFormChange} />
+                            {submitStatus.msg.name ? (
+                                <label className="text-danger">{submitStatus.msg.name}</label>
+                                ): null}
                         </div>
                         <label
-                            className="col-lg-2 col-md-1 col-form-label form-control-label text-light justify-content-end align-items-center d-lg-flex">Email</label>
+                            className="col-lg-2 col-md-1 col-form-label form-control-label text-light justify-content-end align-items-start d-lg-flex">Email</label>
                         <div className="col-md-5 col-lg-4">
                             <input className="form-control mb-lg-2" id="id_email" type="text" name="email"
-                                value={this.email} placeholder="email@gmail.com" onChange={this.handleFormChange} />
+                                value={suggestCourse.email} placeholder="email@gmail.com" onChange={handleFormChange} />
+                            {submitStatus.msg.email ? (
+                                <label className="text-danger">{submitStatus.msg.email}</label>
+                                ): null}
+                            
                         </div>
                     </div>
 
@@ -52,7 +103,10 @@ export default class SuggestCoursePage extends Component {
                             Name</label>
                         <div className="col-lg">
                             <input className="form-control" id="id_course_name" type="text" name="course_name"
-                                placeholder="Django from zero to hero" value={this.courseName} onChange={this.handleFormChange} required />
+                                placeholder="Django from zero to hero" value={suggestCourse.course_name} onChange={handleFormChange} required />
+                            {submitStatus.msg.course_name ? (
+                                <label className="text-danger">{submitStatus.msg.course_name}</label>
+                                ): null}
                         </div>
                     </div>
                     <div className="input-group row mt-2 mt-lg-4">
@@ -61,7 +115,10 @@ export default class SuggestCoursePage extends Component {
                             URL</label>
                         <div className="col-lg-10">
                             <input className="form-control" id="id_course_url" name="course_url" type="text" placeholder=""
-                                value={this.courseURL} onChange={this.handleFormChange} required />
+                                value={suggestCourse.course_url} onChange={handleFormChange} required />
+                            {submitStatus.msg.course_url ? (
+                                <label className="text-danger">{submitStatus.msg.course_url}</label>
+                                ): null}
                         </div>
                     </div>
 
@@ -71,36 +128,37 @@ export default class SuggestCoursePage extends Component {
                         <div className="col-lg-10">
                             <textarea className="form-control" id="id_description" type="text" name="description" required
                                 placeholder="This course will give you a full introduction into all of the core concepts in python."
-                                value={this.description} onChange={this.handleFormChange} />
+                                value={suggestCourse.description} onChange={handleFormChange} />
+                            {submitStatus.msg.description ? (
+                                <label className="text-danger">{submitStatus.msg.description}</label>
+                                ): null}
                         </div>
                     </div>
 
                     <div className="input-group row mt-2 mt-lg-3">
                         <label className="col-lg-2"></label>
                         <div className="col-lg-10">
+                            {submitStatus.isSuccess ?(
+                                <div>
+                                <label style= {{
+                                    // fontSize: "1.2rem",
+                                    fontWeight: "600",
+                                    color: "#1fb075",
+                                    }} className ="pb-3">Thank you for your sharing!</label>
+                                </div>
+                                ): null}
                             <input type="reset" className="btn btn-outline-secondary mr-2" value="Cancel" />
                             <input type="submit" className="btn btn-primary mr-2" name="submit" value="Submit" />
                         </div>
+
                     </div>
                 </form>
 
             </div>
+            </div>
+        </>
+    )
 
-        )
+};
 
-    }
-    render() {
-        return (
-            <>
-                <HeroSectionForSuggest pageName={this.state.pageName} />
-                <div className="container">
-                    {this.renderSuggestCoursePage()}
-                </div>
-            </>
-        )
-    }
-
-    componentDidMount() {
-        document.title = "Course Pool | Suggest Your Favorite Course";
-    }
-}
+export default SuggestCoursePage;
